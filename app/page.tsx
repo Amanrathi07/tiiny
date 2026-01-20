347a942968cdcdfd3fc90c042d1437ee3ec058de
Amanrathi07 <amanrathi4321@gmail.com>
2026-01-20 06:36:57 +0530

    first try

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,48 @@
-import Image from "next/image";
+"use client";
+
+import { authClient } from "@/lib/auth-client"; 
+import { useState } from "react";
+import {Input} from "@/components/ui/input"
+import { Button } from "@/components/ui/button";
+
+
+
 
 export default function Home() {
+
+  async function formHandel(){
+    const { data, error } = await authClient.signUp.email({
+        email, 
+        password,
+        name, 
+      
+    }, {
+        onRequest: (ctx) => {
+            //show loading
+        },
+        onSuccess: (ctx) => {
+            //redirect to the dashboard or sign in page
+        },
+        onError: (ctx) => {
+            // display the error message
+            alert(ctx.error.message);
+        },
+});
+  }
+  
+
+
+  const[name,setName]=useState("");
+  const[password,setPassword]=useState("");
+  const[email,setEmail]=useState("");
+
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-     it working
+      <Input name="name" value={name} onChange={(e)=>setName(e.target.value)}/>
+      <Input name="email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+      <Input name="password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+      <Button onClick={formHandel}>sign Up</Button>
     </div>
   );
 }

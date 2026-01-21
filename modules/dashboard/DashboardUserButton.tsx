f7d7c07f23f28475617b8f2a75011b4d66eb93e7
Amanrathi07 <amanrathi4321@gmail.com>
2026-01-21 14:39:17 +0530

    sidebar compleat

--- modules/dashboard/DashboardUserButton.tsx
+++ modules/dashboard/DashboardUserButton.tsx
@@ -11,14 +11,30 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { GeneratedAvatar } from "@/components/Generated-avatar";
+import { ChevronDownIcon } from "lucide-react";
+import { router } from "better-auth/api";
+import { useRouter } from "next/navigation";
 
 export function DashboardUserButton(){
+    const router = useRouter();
     const {data,isPending} =authClient.useSession();
 
   
     if(isPending || !data?.user){
         return null ;
     }
+
+    async function logout() {
+        await authClient.signOut({
+            fetchOptions:{
+                onSuccess:()=>{
+                    router.push("/sign-in")
+                }
+            }
+        })
+    }
+
     return(
         <DropdownMenu>
             <DropdownMenuTrigger className="rounded-lg border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden ">
@@ -26,8 +42,31 @@ export function DashboardUserButton(){
                     <Avatar>
                         <AvatarImage src={data.user.image} alt="Avtar"/>
                     </Avatar>
-                    ):null}
+                    ):<GeneratedAvatar seed={data.user.name} variant="botttsNeutral" className="size-9 mr-3 "/>}
+
+               <div className="flex flex-col gap-0.5 text-left overflow-hidden flex-1 min-w-0">
+                <p className="text-sm truncate w-full">
+                    {data.user.name}
+                </p>
+                <p className="text-sm truncate w-full">
+                    {data.user.email}
+                </p>
+               </div>     
+            <ChevronDownIcon />
             </DropdownMenuTrigger>
+            <DropdownMenuContent align="end" side="right" className="w-72">
+                <DropdownMenuLabel>
+                   <div className="flex flex-col gap-1">
+                     <span className="font-medium truncate">{data.user.name}</span>
+                    <span className="text-sm font-normal text-muted-foreground truncate">{data.user.email}</span>
+                   </div>
+                </DropdownMenuLabel>
+
+                <DropdownMenuSeparator />
+
+                <DropdownMenuItem className="cursor-pointer flex items-center justify-between">Billing</DropdownMenuItem>
+                <DropdownMenuItem className="cursor-pointer flex items-center justify-between" onClick={logout}>Logout</DropdownMenuItem>
+            </DropdownMenuContent>
         </DropdownMenu>
     )
  }

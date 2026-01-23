a58f86293918eb923e600a7a2263adce02f7e770
Amanrathi07 <amanrathi4321@gmail.com>
2026-01-23 18:26:44 +0530

    interve bad

--- modules/navbar/DashboardNavbar.tsx
+++ modules/navbar/DashboardNavbar.tsx
@@ -1,9 +1,27 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { useSidebar } from "@/components/ui/sidebar";
+import { PanelLeftClose, PanelLeftIcon , SearchIcon} from "lucide-react";
+
 export  function DashboardNavbar() {
+    const {state , toggleSidebar ,isMobile} = useSidebar()
+
   return (
     <nav className="flex px-4 gap-x-2 items-center py-3 border-b bg-background">
-        hello
+        <Button className="size-9" variant="outline" onClick={toggleSidebar}>
+            {(state==="collapsed"||isMobile)
+            ?<PanelLeftIcon   className="size-4"  />
+            :<PanelLeftClose  className="size-4"  />}
+        </Button>
+        <Button className="h-9 w-[240px ] justify-start font-normal text-muted-foreground hover:text-muted-foreground" variant="outline" size="sm" onClick={()=>{}} >
+            <SearchIcon />
+            search
+            <kbd>
+                span
+            </kbd>
+        </Button>
+        
     </nav>
   )
 }
